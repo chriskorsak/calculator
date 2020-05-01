92b675db6c170ffd94481c5f5bb74f74307572f0
chriskorsak <chris@chriskorsak.com>
2020-04-30 22:19:58 -0700

    remove multiple forms and condense into one form with radio options for + - * /. update jquery with branching logic and update jquery selectors.

--- js/scripts.js
+++ js/scripts.js
@@ -20,37 +20,21 @@ var divide = function(number1, number2) {
 
 $(document).ready(function() {
 
-  $("form#add").submit(function(event) {
-    console.log(event);
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#addOutput").text(result);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2)
+    }
+    document.getElementById("output").innerHTML = result;
   });
-
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#subtract1").val());
-    var number2 = parseInt($("#subtract2").val());
-    var result = subtract(number1, number2);
-    $("#subtractOutput").text(result);
-  });
-
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#multiply1").val());
-    var number2 = parseInt($("#multiply2").val());
-    var result = multiply(number1, number2);
-    $("#multiplyOutput").text(result);
-  });
-
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var result = divide(number1, number2);
-    $("#divideOutput").text(result);
-  });
-
 });
